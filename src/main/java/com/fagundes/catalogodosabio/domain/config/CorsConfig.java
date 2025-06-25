e7771b7668a2f1d95dba178634f9abec011f6487
welington <welingtonfagundesch@gmail.com>
2025-06-25 19:49:11 -0300

    adicionando classe de paginacao

--- src/main/java/com/fagundes/catalogodosabio/domain/config/CorsConfig.java
+++ src/main/java/com/fagundes/catalogodosabio/domain/config/CorsConfig.java
@@ -13,8 +13,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+                        .allowedOrigins("http://localhost:4200", "http://localhost:8080")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
